ab24cc33e514c9e2f33fc15819fd647a0a214e6d
Jeppe Vad Andersen <Jeppe>
2025-06-06 22:49:14 +0200

    Formatting

--- flake.nix
+++ flake.nix
@@ -5,22 +5,25 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs, ...}: let
+  outputs = { self, nixpkgs, ...}:
+  let
     system = "x86_64-linux";
   in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in pkgs.mkShell.override {
-      stdenv = pkgs.clang16Stdenv;
-    } {
+    devShells."${system}".default =
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; }
+    {
       nativeBuildInputs = with pkgs; [
         pkg-config
+      ];
 
+      buildInputs = with pkgs; [
         meson
         ninja
         SDL2
+        glibc
+        cmake
 
         zsh
       ];
